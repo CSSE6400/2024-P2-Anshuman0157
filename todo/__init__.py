85be0542f529ed35359d5bbdabe4da40b06b3a18
Anshuman Misra <108968309+Anshuman0157@users.noreply.github.com>
2024-03-13 13:53:38 +0000

    Test

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
+def create_app(config_overrides = None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
+    if config_overrides:
+        app.config.update(config_overrides)
+
     from todo.models import db
     from todo.models.todo import Todo
-
-    
-
     db.init_app(app)
     
 
